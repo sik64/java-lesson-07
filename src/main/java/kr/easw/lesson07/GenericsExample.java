f6b0f4443a4228faa86ce9283c45d86749f56ad1
3uns1k <93184550+sik64@users.noreply.github.com>
2024-05-09 13:54:46 +0900

    Update GenericsExample.java

--- src/main/java/kr/easw/lesson07/GenericsExample.java
+++ src/main/java/kr/easw/lesson07/GenericsExample.java
@@ -10,7 +10,7 @@ package kr.easw.lesson07;
 public class GenericsExample {
 
     public static void main(String[] args) {
-        Recipe<Ingredient, Cake> recipe = new Recipe<>(new RottenEgg()) {
+        Recipe<Ingredient, Cake> recipe = new Recipe<>(new Egg()) {
             @Override
             public Cake cook() {
                 // 제너릭스를 통해 클래스를 확정했음으로, T 제너릭스가 Ingredient를 상속받는것이 보장됩니다.
@@ -60,7 +60,8 @@ public class GenericsExample {
     static class RottenEgg implements Ingredient {
         @Override
         public void mix() {
-            throw new RuntimeException("Rotten egg detected!");
+            System.out.println("Mixing rotten...");
+            //throw new RuntimeException("Rotten egg detected!");
         }
     }
 
